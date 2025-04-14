ae9c3f6c6b86f014ee182d28da4b82399b41a3c0
Stephen Appah <appahstephen9@gmail.com>
2025-04-14 02:23:53 +0000

    removed unused import

--- src/app/Authentication/signup/signup.component.ts
+++ src/app/Authentication/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../Service/auth.service';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
