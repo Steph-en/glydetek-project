889e94a857b5e72ab414aff1b32662068d5ed0fa
Stephen Appah <appahstephen9@gmail.com>
2025-04-14 02:16:08 +0000

    environment variables (firebase)

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBCNcmzln3kyCxA_yYeHzIsqus3AXg703M",
+        authDomain: "glydetek-project.firebaseapp.com",
+        projectId: "glydetek-project",
+        storageBucket: "glydetek-project.firebasestorage.app",
+        messagingSenderId: "410296912493",
+        appId: "1:410296912493:web:700ce9a164de681e5b8d64",
+        measurementId: "G-NMS9K50CSK"
+    }
+};
