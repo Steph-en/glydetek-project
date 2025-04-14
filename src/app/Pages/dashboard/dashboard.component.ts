972ebffdd12514f486604608e67cbb0d7147960f
Stephen Appah <appahstephen9@gmail.com>
2025-04-14 02:15:14 +0000

    dashboard component

---
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from '../../Service/auth.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [AsyncPipe, CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent implements OnInit {
+  private authService = inject(AuthService);
+  currentUser$ = this.authService.currentUser$;
+
+  ngOnInit(): void {
+    // The currentUser$ observable in AuthService will emit the user profile
+  }
+
+  async logout(): Promise<void> {
+    await this.authService.logout();
+  }
+}
