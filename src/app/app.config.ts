68bf5912b1cb81e026b476f3baa4e43211ffc8c6
Stephen Appah <appahstephen9@gmail.com>
2025-04-14 02:17:47 +0000

    app configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './Service/auth.service';
+import { AuthGuard } from './Authentication/auth.guard';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    AuthService,
+    {
+      provide: 'canActivateDashboard', // You can use a string token
+      useValue: AuthGuard,
+      multi: true, // If multiple guards can apply to a route
+    },
+  ],
 };
