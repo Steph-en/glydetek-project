99b956f45ef04687ba152c1bd54892e2bd254201
Stephen Appah <appahstephen9@gmail.com>
2025-04-14 02:18:07 +0000

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Authentication/login/login.component';
+import { SignupComponent } from './Authentication/signup/signup.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { AuthGuard } from './Authentication/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        redirectTo: '/login', 
+        pathMatch: 'full', 
+    },
+    { 
+        path: 'login', 
+        component: LoginComponent, 
+    },
+    { 
+        path: 'signup', 
+        component: SignupComponent, 
+    },
+    { path: 
+        'dashboard', 
+        component: DashboardComponent, 
+        canActivate: [AuthGuard] 
+    },
+];
